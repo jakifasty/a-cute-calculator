7a8512a9eb86ef36507e8a9ed1da16901f4781b6
Borja Javierre <borja.general@gmail.com>
2023-02-08 21:31:46 +0100

    fix OperationButton component and DOM tree

--- src/DigitButton.js
+++ src/DigitButton.js
@@ -1,6 +1,6 @@
 import {ACTIONS} from './App'
 
-export default function DigitButton({ dispatch, digit }){
+export default function DigitButton({ digit, dispatch }){
     return (
         <button 
             onClick={() => dispatch({type: ACTIONS.ADD_DIGIT, payload: { digit }})}>
--- src/OperationButton.js
+++ src/OperationButton.js
@@ -1,10 +1,10 @@
 import {ACTIONS} from './App'
 
-export default function OperationButton({ dispatch, digit }){
+export default function OperationButton({operation, dispatch}){
     return (
         <button 
-            onClick={() => dispatch({type: ACTIONS.ADD_DIGIT, payload: { digit }})}>
-            {digit} 
+            onClick={() => dispatch({type: ACTIONS.CHOOSE_OPERATION, payload: { operation }})}>
+            {operation} 
         </button>
     )
 }
