64077d97f4233adcbb70c92a45aa40b38ff5c4a4
Borja Javierre <borja.general@gmail.com>
2023-02-10 21:42:34 +0100

    fix calculator logic

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ export const ACTIONS = { //action break into actions with types and parameters
   EVALUATE: 'evaluate'
 }
 
-function reducer(state, {type, payload }){
+function reducer(state, {type, payload }){ //this includes all the excepions and logic when clicking buttons
   switch(type){
     case ACTIONS.ADD_DIGIT:
-      if (payload.digit === "0" && state.currentOperand === "0") {return state}
-      if (payload.digit === "." && state.currentOperand.includes(".")) {return state}
+      if (payload.digit === "0" && state.currentOperand === "0") return state
+      if (payload.digit === "." && state.currentOperand.includes(".")) return state
 
       return {
         ...state,
@@ -24,17 +24,21 @@ function reducer(state, {type, payload }){
     
     case ACTIONS.CLEAR:
       return {}
-    
-    case ACTIONS.CHOOSE_OPERATION:
-      if (state.currentOperand == null && state.previousOperand == null){ //if no number before operation
+    case ACTIONS.CHOOSE_OPERATION: //if no number before operation
+      if (state.currentOperand == null && state.previousOperand == null)
         return state //return state
-      }
       if (state.previousOperand == null){ //after typing numbers, we need to add operation and place it in 
         return {
           ...state,
           operation: payload.operation,
           previousOperand: state.currentOperand,
           currentOperand: null,
+        }
+      }
+      if (state.currentOperand == null){
+        return {
+          ...state,
+          operation: payload.operation
         }
       }
       return{ //default action
@@ -45,23 +49,33 @@ function reducer(state, {type, payload }){
       }
     case ACTIONS.DELETE_DIGIT:
       return {}
+    case ACTIONS.EVALUATE:
+      if (state.currentOperand == null || state.previousOperand == null || state.operation == null){
+        return state
+      }
+      return{
+        ...state,
+        previousOperand: null,
+        operation: null,
+        currentOperand: evaluate(state),
+      }
   }
 }
 
-function evaluate({currentOperand, previousOperand, operation}){
+function evaluate({currentOperand, previousOperand, operation}){ // state -> {currentOperand, previousOperand, operation}; this function implements all the logic for making the operations
   const prev = parseFloat(previousOperand)
   const current = parseFloat(currentOperand)
-  if (isNaN(prev) || isNaN(current)) return ""
+  if (isNaN(prev) || isNaN(current)) return "" //return empty string
   let computation = ""
   switch (operation){
     case "+":
-      computation = prev + current
+      computation = prev + current; break
     case "-":
-      computation = prev - current
+      computation = prev - current; break
     case "/":
-      computation = prev / current
+      computation = prev / current; break
     case "*":
-      computation = prev * current
+      computation = prev * current; break
   }
 
   return computation.toString()
@@ -98,7 +112,7 @@ function App() {
 
       <DigitButton digit="." dispatch={dispatch} />
       <DigitButton digit="0" dispatch={dispatch} />
-      <button className="span-two">=</button>
+      <button className="span-two" onClick={() => dispatch({type: ACTIONS.EVALUATE})}>=</button>
     </div>
   );
 }
